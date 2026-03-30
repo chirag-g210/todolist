44f256282b509f75ef65ac9ece5ffc1cd4cb67af
Chirag Gupta <piyushg.2648@gmail.com>
2026-03-30 16:39:09 +0530

    Add todolist app files

---
+++ todolist/script.js
@@ -0,0 +1,23 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let task = input.value;
+
+  if (task === "") return;
+
+  let li = document.createElement("li");
+  li.innerHTML = `
+    <span onclick="toggleTask(this)">${task}</span>
+    <button onclick="deleteTask(this)">❌</button>
+  `;
+
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
+
+function deleteTask(button) {
+  button.parentElement.remove();
+}
+
+function toggleTask(task) {
+  task.parentElement.classList.toggle("completed");
+}
